e049f93d6f2f7f44062d41e081d845b75b559032
unknown <j.mossink@datawizards.nl>
2019-11-27 17:12:53 +0100

    working version firebase extraction

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    {{dailies}}
+  </div>
+</template>
+
+<script>
+import { db } from '../firebase'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      dailies: []
+    }
+  },
+  firestore () {
+    return {
+      dailies: db.collection('workout_types')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,25 +1,11 @@
 import Vue from 'vue'
-import firebase from 'firebase'
 import App from './App'
 import router from './router'
+import VueFirestore from 'vue-firestore'
 
 Vue.config.productionTip = false
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDuN2TJ8SoMX8lKqdtIYYFHjtI7kV8Fu6c",
-    authDomain: "trainingtrackr.firebaseapp.com",
-    databaseURL: "https://trainingtrackr.firebaseio.com",
-    projectId: "trainingtrackr",
-    storageBucket: "trainingtrackr.appspot.com",
-    messagingSenderId: "200244850367",
-    appId: "1:200244850367:web:7d3d507d5892b962a6200a",
-    measurementId: "G-6FRCXMS0QY"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig)
-  firebase.analytics()
-
+Vue.use(VueFirestore)
 
 /* eslint-disable no-new */
 new Vue({
