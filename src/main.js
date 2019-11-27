85f047a2995c2076b0768c9b8e286a7a4c249826
Anouk Boukema <anouk@madx.nl>
2019-11-27 21:28:49 +0100

    including bootstrap

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import VueFirestore from 'vue-firestore'
 import BootstrapVue from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
-Vue.use(VueFirestore, BootstrapVue)
+Vue.use(VueFirestore)
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
