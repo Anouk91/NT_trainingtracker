35f744a6316fa9ede8efbf7973ff83313757018c
Anouk91 <boukema.a@gmail.com>
2019-12-04 11:31:22 +0100

    locally added users NTD

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,16 +4,19 @@
     <div style="margin: 20px;">
       <label>Selecteer een user</label>
       <select>
-        <option :key="user.id"
-        v-for="user in users"
-        value="user.name" >
-        {{user.name}}
+        <option
+          :key="user.id"
+          v-for="user in users_local"
+          value="user.firstname"
+        >
+          {{ user.firstname }}
         </option>
       </select>
       <div class="logging_grid">
-        <button v-for="type in workout_types" :key="type.id">{{type.name}}</button>
+        <button v-for="type in workout_types" :key="type.id">
+          {{ type.firstname }}
+        </button>
       </div>
-
     </div>
   </div>
 </template>
@@ -28,7 +31,72 @@ export default {
     return {
       users: [],
       workout_types: [],
-      today: new Date()
+      today: new Date(),
+      users_local: [
+        {
+          id: '77nRofkS0AlQ1u16NwPv',
+          firstname: 'Anna',
+          lastname: 'Hoefnagels'
+        },
+        {
+          id: 'A4kUyv2JPhcTJSTtHMjF',
+          firstname: 'Anniek',
+          lastname: 'Leinenga'
+        },
+        { id: '5m6HhvhJ7hHE6IcKRgTT', firstname: 'Anouk', lastname: 'Boukema' },
+        {
+          id: '8t1ykXymRJBLsRXGocM6',
+          firstname: 'Britt',
+          lastname: 'de Bruijn'
+        },
+        {
+          id: 'bLSAx8lL0XNmYPYz62B7',
+          firstname: 'Charlotte',
+          lastname: 'Verheij'
+        },
+        {
+          id: '38W7Udb7iZwrGFYRMcI6',
+          firstname: 'Camilla',
+          lastname: 'van Wirdum'
+        },
+        { id: 'ILbIQ0BIeJXYonsqwJZw', firstname: 'Eline', lastname: 'Wilhelm' },
+        { id: 'HbYi5oIsSkMvTv6HzuTL', firstname: 'Iris', lastname: 'Leinenga' },
+        { id: 'z0t4LTzzAo763VavIQju', firstname: 'Iris', lastname: 'Terpstra' },
+        {
+          id: '0qJT3xW1BFdCcIDhrktA',
+          firstname: 'Jill',
+          lastname: 'Steenkist'
+        },
+        { id: 'a1n0QQuDnwJBo7xjpHj0', firstname: 'Joske', lastname: 'Brouwer' },
+        { id: 'MmQygeTZkmNaQEyPodh3', firstname: 'Linde', lastname: 'Wits' },
+        { id: 'zm9fjuG9UwFhTSA8vTIm', firstname: 'Lisa', lastname: 'Wohlrab' },
+        {
+          id: 'R6IYa0zKzkCgdR4gRj6S',
+          firstname: 'Mirte',
+          lastname: 'van der Lee'
+        },
+        { id: '8UEoDBL3KN5uDftXZKns', firstname: 'Nena', lastname: 'Ruijs' },
+        { id: 'ux2Sx19OXDq2yEblPbOO', firstname: 'Paula', lastname: 'Baas' },
+        {
+          id: 'H8Ag78e9m4U0kuiPnE0H',
+          firstname: 'Roos',
+          lastname: 'Maaike Denisse'
+        },
+        { id: 'AockYXZeoYf1xGOHjewS', firstname: 'Sarah', lastname: 'Sparks' },
+        {
+          id: '4gpRUiuVO1vknaeSg3SF',
+          firstname: 'Suzanne',
+          lastname: 'Delwel'
+        },
+        {
+          id: 'bYbtfXX2oKI6gYMiiolH',
+          firstname: 'Tess',
+          lastname: 'van Middelaar'
+        },
+        { id: 'cXmkW4LBc9SaFZRXWLeR', firstname: 'Tirza', lastname: 'Moerman' },
+        { id: 'rpVd9PoCTsFuKbQ98aZ3', firstname: 'Julia', lastname: 'Ose' },
+        { id: 'gGxzMHEDmyy7Rr92Aycc', firstname: 'Rixt', lastname: 'Hofman' }
+      ]
     }
   },
   firestore () {
@@ -66,7 +134,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
@@ -81,7 +150,7 @@ li {
 }
 
 a {
-  color: #35495E;
+  color: #35495e;
 }
 
 .logging_grid {
@@ -99,9 +168,17 @@ button {
   background-color: lightblue;
   font-size: 2rem;
   color: #333;
-  background-image: linear-gradient(to bottom,transparent,transparent 50%,rgba(0,0,0,.04));
-  box-shadow: inset 0 0 0 1px rgba(255,255,255,.05), inset 0 1px 0 0 rgba(255,255,255,.45), inset 0 -1px 0 0 rgba(255,255,255,.15), 0 1px 0 0 rgba(255,255,255,.15);
-  text-shadow: 0 1px rgba(255,255,255,.4);
+  background-image: linear-gradient(
+    to bottom,
+    transparent,
+    transparent 50%,
+    rgba(0, 0, 0, 0.04)
+  );
+  box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.05),
+    inset 0 1px 0 0 rgba(255, 255, 255, 0.45),
+    inset 0 -1px 0 0 rgba(255, 255, 255, 0.15),
+    0 1px 0 0 rgba(255, 255, 255, 0.15);
+  text-shadow: 0 1px rgba(255, 255, 255, 0.4);
 }
 
 button:hover {
