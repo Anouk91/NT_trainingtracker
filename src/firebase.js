acf235de38be3bff903bfa77a18832f4a372c71f
Anouk Boukema <anouk@madx.nl>
2020-01-03 17:37:08 +0100

    right db

--- src/firebase.js
+++ src/firebase.js
@@ -2,28 +2,28 @@ import { firebase } from '@firebase/app'
 import '@firebase/firestore'
 
 // // Testing
-const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyDuN2TJ8SoMX8lKqdtIYYFHjtI7kV8Fu6c',
-  authDomain: 'trainingtrackr.firebaseapp.com',
-  databaseURL: 'https://trainingtrackr.firebaseio.com',
-  projectId: 'trainingtrackr',
-  storageBucket: 'trainingtrackr.appspot.com',
-  messagingSenderId: '200244850367',
-  appId: '1:200244850367:web:7d3d507d5892b962a6200a',
-  measurementId: 'G-6FRCXMS0QY'
-})
+// const firebaseApp = firebase.initializeApp({
+//   apiKey: 'AIzaSyDuN2TJ8SoMX8lKqdtIYYFHjtI7kV8Fu6c',
+//   authDomain: 'trainingtrackr.firebaseapp.com',
+//   databaseURL: 'https://trainingtrackr.firebaseio.com',
+//   projectId: 'trainingtrackr',
+//   storageBucket: 'trainingtrackr.appspot.com',
+//   messagingSenderId: '200244850367',
+//   appId: '1:200244850367:web:7d3d507d5892b962a6200a',
+//   measurementId: 'G-6FRCXMS0QY'
+// })
 
 // Productie
-// const firebaseProductionApp = firebase.initializeApp({
-//   apiKey: 'AIzaSyA4pesZDgREDjWVUpVz_jPcBZ65hcxkh8E',
-//   authDomain: 'trainingtrackr-productie.firebaseapp.com',
-//   databaseURL: 'https://trainingtrackr-productie.firebaseio.com',
-//   projectId: 'trainingtrackr-productie',
-//   storageBucket: 'trainingtrackr-productie.appspot.com',
-//   messagingSenderId: '798479808372',
-//   appId: '1:798479808372:web:d3aeb7d3833f70ff981e21',
-//   measurementId: 'G-V7C0D5BL3S'
-// })
+const firebaseProductionApp = firebase.initializeApp({
+  apiKey: 'AIzaSyA4pesZDgREDjWVUpVz_jPcBZ65hcxkh8E',
+  authDomain: 'trainingtrackr-productie.firebaseapp.com',
+  databaseURL: 'https://trainingtrackr-productie.firebaseio.com',
+  projectId: 'trainingtrackr-productie',
+  storageBucket: 'trainingtrackr-productie.appspot.com',
+  messagingSenderId: '798479808372',
+  appId: '1:798479808372:web:d3aeb7d3833f70ff981e21',
+  measurementId: 'G-V7C0D5BL3S'
+})
 firebase.firestore().enablePersistence().catch(function (err) {
   if (err.code === 'failed-precondition') {
     console.log('err', err)
@@ -37,4 +37,4 @@ firebase.firestore().enablePersistence().catch(function (err) {
     // ...
   }
 })
-export const db = firebaseApp.firestore()
+export const db = firebaseProductionApp.firestore()
